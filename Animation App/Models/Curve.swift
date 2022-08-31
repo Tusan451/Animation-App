1e5765d827e6cec716863d8488c4cd2e78ac0745
Oleg Fedotov <trevor706@gmail.com>
2022-08-31 19:43:26 +0300

    Curve model

---
+++ Animation App/Models/Curve.swift
@@ -0,0 +1,16 @@
+//
+//  Curve.swift
+//  Animation App
+//
+//  Created by Olegio on 31.08.2022.
+//
+
+import Foundation
+
+class Curve {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
