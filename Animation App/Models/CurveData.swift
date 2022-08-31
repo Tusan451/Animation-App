acd4d53ccbe633298d98a76f3196f0fcb1216953
Oleg Fedotov <trevor706@gmail.com>
2022-08-31 19:53:01 +0300

    CurveData model

---
+++ Animation App/Models/CurveData.swift
@@ -0,0 +1,43 @@
+//
+//  CurveData.swift
+//  Animation App
+//
+//  Created by Olegio on 31.08.2022.
+//
+
+import Foundation
+import Spring
+
+class CurveData {
+    var curves: [Spring.AnimationCurve] = [
+        .EaseIn,
+        .EaseOut,
+        .EaseInOut,
+        .Linear,
+        .Spring,
+        .EaseInSine,
+        .EaseOutSine,
+        .EaseInOutSine,
+        .EaseInQuad,
+        .EaseOutQuad,
+        .EaseInOutQuad,
+        .EaseInCubic,
+        .EaseOutCubic,
+        .EaseInOutCubic,
+        .EaseInQuart,
+        .EaseOutQuart,
+        .EaseInOutQuart,
+        .EaseInQuint,
+        .EaseOutQuint,
+        .EaseInOutQuint,
+        .EaseInExpo,
+        .EaseOutExpo,
+        .EaseInOutExpo,
+        .EaseInCirc,
+        .EaseOutCirc,
+        .EaseInOutCirc,
+        .EaseInBack,
+        .EaseOutBack,
+        .EaseInOutBack
+    ]
+}
