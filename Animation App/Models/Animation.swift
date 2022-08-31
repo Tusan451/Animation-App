c32b93f1bb54ba6ca1016629f4dd25233c851c3d
Oleg Fedotov <trevor706@gmail.com>
2022-08-31 19:41:26 +0300

    Animation model

---
+++ Animation App/Models/Animation.swift
@@ -0,0 +1,16 @@
+//
+//  Animation.swift
+//  Animation App
+//
+//  Created by Olegio on 31.08.2022.
+//
+
+import Foundation
+
+class Animation {
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
